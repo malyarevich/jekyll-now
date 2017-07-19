d06ed8e7d92db5c340914c3b42d07c1fa92eca86
Paul Malyarevich <paul.lightit@gmail.com>
2017-07-19 16:10:12 +0300

    header

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 jQuery( document ).ready(function () {
-
+    jQuery(".menu-box>button").click(function () {
+        if ( jQuery(".menu-box__open").is(":visible") ) {
+            jQuery(".menu-box__open").hide();
+            jQuery(".menu-box__hamb").show();
+        } else {
+            jQuery(".menu-box__open").hide();
+            jQuery(".menu-box__hamb").show();
+        }
+    });
 });
