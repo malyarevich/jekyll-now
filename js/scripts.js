55f25b032b8f3fda964406b8d2b28811d9f71d76
Paul Malyarevich <paul.lightit@gmail.com>
2017-07-19 16:48:55 +0300

    header

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 jQuery( document ).ready(function () {
     jQuery(".menu-box button").click(function () {
-        jQuery(".menu-box__open").toggle();
-        jQuery(".menu-box__hamb").toggle();
         if ( !jQuery(".menu-box__open").is(":visible") ) {
-            jQuery(".menu-box").css("background-color", "rgb(80, 134, 185)");
+            jQuery(".menu-box__hamb").show();
+            jQuery(".menu-box__open").hide();
+            jQuery(".menu-box").css("background-color", "rgba(80, 134, 185, 1)");
         } else {
+            jQuery(".menu-box__hamb").hide();
+            jQuery(".menu-box__open").show();
             jQuery(".menu-box").css("background-color", "rgba(80, 134, 185, 0)");
         }
     });
