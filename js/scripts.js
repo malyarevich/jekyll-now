3f23894fe8b90eff752b1fa12ad8de9e0c2736d7
Paul Malyarevich <paul.lightit@gmail.com>
2017-07-19 16:23:55 +0300

    header

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 jQuery( document ).ready(function () {
     jQuery(".menu-box>button").click(function () {
         if ( jQuery(".menu-box__open").is(":visible") ) {
+            jQuery(".menu-box").css("background-color", "#5086B9");
             jQuery(".menu-box__open").hide();
             jQuery(".menu-box__hamb").show();
         } else {
+            jQuery(".menu-box").css("background-color", "none");
             jQuery(".menu-box__open").show();
             jQuery(".menu-box__hamb").hide();
         }
